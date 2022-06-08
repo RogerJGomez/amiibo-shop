dacfdc71fae5c450d25d9f046bfd6074bc464a71
Roger Gómez <gomez.roger779@gmail.com>
2022-06-08 12:34:18 -0500

    Added font family JosefinSan

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,12 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        josefinSemiBold: ['JosefinSans-SemiBold', 'sans-serif'],
+        josefin: ['JosefinSans-Regular', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
